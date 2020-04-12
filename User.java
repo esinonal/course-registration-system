71b193ef2b3cc438d1c8e040d4d7e0f17bd82b8d
esinonal <46625198+esinonal@users.noreply.github.com>
2020-04-13 00:29:55 +0200

    Update User.java

--- User.java
+++ User.java
@@ -1,20 +1,14 @@
-
-
 public class User implements java.io.Serializable {
 	
-	protected String username = "initializetousernamethatwillnotbeused";   //initialize to a username that will not be used just to initialize it
+	protected String username = "initializetousernamethatwillnotbeused";   //initialize to a username that will not be used
 	protected String password;
 	protected String firstName;
 	protected String lastName;
 	
-	
-	
 	User() {
-		
 	}
 	
 	public String getUsername() {
 		return username;
 	}
-
 }
